0e2d8030cb06698b0d861a5e0acd2ac9a47164d3
Sona2603 <sonanagarajan076@gmail.com>
2026-06-28 15:06:44 +0530

    Update API URLs for production

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 import { toast, ToastContainer }
 from "react-toastify";
 
